52329e85897573b0f29a6a13e979bd99b27d107e
Csizmadia Márk <airamek@teamorange.hu>
2024-02-12 14:08:09 +0100

    Add project files.

---
+++ SZGYA-Villámlások-20240212/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using SZGYA_Villámlások_20240212.src;
+
+namespace SZGYA_Villámlások_20240212
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<VillamNap> villamnapok = new List<VillamNap>();
+
+            StreamReader sr = new StreamReader("../../../src/villam.txt", Encoding.UTF8);
+
+            for(int i = 1; !sr.EndOfStream; i++)
+            {
+                villamnapok.Add(new VillamNap(i, sr.ReadLine()));
+            }
+
+            sr.Close();
+
+            //2
+            Console.WriteLine("\n2.feladat");
+            foreach (var v in villamnapok)
+            {
+                Console.WriteLine(v);
+            }
+            //3
+            var maxnap = villamnapok.MaxBy(v => v.Orak.Max());
+            var maxora = maxnap.Orak.IndexOf(maxnap.Orak.Max()) + 1;
+            Console.WriteLine($"\n3.feladat \n\t{maxnap.Nap}. nap {maxora} óra");
+
+            //4
+            Console.WriteLine("\n4.feladat");
+            StreamWriter sw = new StreamWriter("../../../src/villamkezdet.txt", false, Encoding.UTF8);
+            foreach (var nap in villamnapok)
+            {
+                var villamlas = nap.Orak.FirstOrDefault(n => n != 0);
+                sw.WriteLine($"{nap.Nap} {(villamlas == 0 ? "null" : nap.Orak.IndexOf(villamlas) + 1)}");
+            }
+            sw.Close();
+
+            //5
+            Console.WriteLine($"\n5.feladat: {villamnapok.Sum(v => v.Orak.Count(n => n > 0))}");
+
+            //6
+            Console.WriteLine($"\n6.feladat: {villamnapok.First(v => v.Orak.Sum() < 200).Nap}");
+
+            //7
+            var nemvoltnap = villamnapok.FirstOrDefault(v => v.Orak.Sum() == 0);
+            Console.WriteLine($"\n7.feladat: {(nemvoltnap == null ? "[HIBA] nem volt ilyen nap" : nemvoltnap.Nap)}");
+
+            //--------------------
+            //------SZORGALMI-----
+            //--------------------
+
+            //8
+            Console.WriteLine($"\n8.feladat: {villamnapok.Sum(v => v.Orak.Chunk(2).First().Count(n => n > 0))}");
+
+            //9
+            Console.WriteLine($"\n9.feladat: {villamnapok.Chunk(20).First().Sum(v => v.Orak.Sum())}");
+
+            //10 - nincs kész
+            var min = villamnapok.MinBy(v => v.Orak.Min());
+            Console.WriteLine($"\n10.feladat: {min}");
+        }
+    }
+}
---
+++ SZGYA-Villámlások-20240212/src/VillamNap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGYA_Villámlások_20240212.src
+{
+    internal class VillamNap
+    {
+        public int Nap { get; set; }
+        public List<int> Orak { get; set; }
+
+        public VillamNap(int nap, string orak)
+        {
+            this.Nap = nap;
+            this.Orak = new List<int>();
+            string[] nyers = orak.Split(';');
+            foreach (string s in nyers) 
+            {
+                this.Orak.Add(int.Parse(s));
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" ", Orak);
+        }
+
+    }
+}
